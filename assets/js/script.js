848b4d3ab036453af9d981b33b769b96fad14606
Zach Weston <zachweston@Zachs-MacBook-Pro.local>
2022-10-29 19:42:38 -0600

    created character arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 // Assignment Code
+
+// Declaring arrays with all possible values for different character types (lowercase letters, numbers, etc.)
+
+const lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "=", "<", ">", "/", ",", "."];
+
+
+// Store a reference to the "generate password" button
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
