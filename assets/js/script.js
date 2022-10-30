41c4ca5734f97ffc8f4285c5d5406cffb6fc1296
Zach Weston <zachweston@Zachs-MacBook-Pro.local>
2022-10-29 19:50:01 -0600

    obtained desired password length/character types from user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,6 @@ const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{"
 
 
 // Store a reference to the "generate password" button
-
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -23,3 +22,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+    // Variable receives the value the user inputs when answering the prompt
+    let length = prompt("Please enter the number of characters you'd like in your password. Make sure the number is at least 8 and no more than 128.");
+    // If the user's answer isn't a number or doesnt fall into the right range, they will be reminded of the password criteria
+    if (isNaN(length) || length < 8 || length > 128) {
+        alert("Remember to only enter a number between 8 and 128! This will determine the length of your password.");
+        return "";
+    }
+    // Variables store boolean values for user's response to each of the following prompts
+    let lowerCaseBoolean = confirm("Would you like to include lowercase letters in your password?");
+    let upperCaseBoolean = confirm("Would you like to include uppercase letters in your password?");
+    let numberBoolean = confirm("Would you like to include numbers in your password?");
+    let specialCharactersBoolean = confirm("Would you like to include special characters in your password?");
+    // If the user doesn't choose any character types, they will be reminded to choose at least one in order to move forward
+}
